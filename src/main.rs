895d0b8d349daf1d27328987d3e65324cbe2e40c
Peter Jacobs <peter@crespyl.net>
2019-11-27 01:18:10 -0600

    leave qml resources outside of the binary

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,18 @@ mod ui;
 mod roku;
 mod discovery;
 
-qrc!(init_qml_resources,
-     "ui" {
-         "ui/remote.qml" as "remote.qml",
-         "ui/RemoteTab.qml" as "RemoteTab.qml",
-         "ui/ChannelTab.qml" as "ChannelTab.qml",
-     }
-);
+// qrc!(init_qml_resources,
+//      "ui" {
+//          "ui/remote.qml" as "remote.qml",
+//          "ui/RemoteTab.qml" as "RemoteTab.qml",
+//          "ui/ChannelTab.qml" as "ChannelTab.qml",
+//      }
+// );
 
 /// This function initializes the qrc resources (QML files, images, etc) and
 /// registers our RokuRemote type so that we can call its methods from QML.
 pub fn init_ui() {
-    init_qml_resources();
+    //init_qml_resources();
     qml_register_type::<ui::RokuRemote>(
         CStr::from_bytes_with_nul(b"RokuRemote\0").unwrap(),
         0, 1,
@@ -35,7 +35,8 @@ pub fn init_ui() {
 fn main() {
     init_ui();
     let mut engine = QmlEngine::new();
-    engine.load_file("qrc:/ui/remote.qml".into());
+    // engine.load_file("qrc:/ui/remote.qml".into());
+    engine.load_file("ui/remote.qml".into());
     engine.exec();
 }
 
